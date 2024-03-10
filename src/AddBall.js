72b60db384cbbe697e9f3e3cb0880036c8825c4d
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-03-10 00:05:23 -0800

    update AddBall.js to have a form

--- src/AddBall.js
+++ src/AddBall.js
@@ -1,7 +1,63 @@
+import React, { useState } from 'react'
+
 function AddBall() {
+    const [ballForm, setBallForm] = useState({
+        name: "",
+        year: "",
+        competition: "",
+        image: ""
+    })
+    console.log(ballForm)
+
+    function handleInputChange(event) {
+        setBallForm({
+            ...ballForm,
+            [event.target.name]: event.target.value
+        })
+    }
+    
     return (
         <div>
-            <p>This is the add ball page</p>
+            <h2>Your favorite ball not on the list? Add it:</h2>
+            <br />
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input 
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="ball name"
+                    value={ballForm.name}
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="year">Year: </label>
+                <input 
+                    type="text"
+                    name="year"
+                    id="year"
+                    placeholder="year made"
+                    value={ballForm.year}
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="competition">Competition: </label>
+                <input 
+                    type="text"
+                    name="competition"
+                    id="competition"
+                    placeholder="competition name"
+                    value={ballForm.competition}
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="image">Image: </label>
+                <input 
+                    type="text"
+                    name="image"
+                    id="image"
+                    placeholder="image url"
+                    value={ballForm.image}
+                    onChange={handleInputChange}
+                />
+            </form>
         </div>
     )
 }
